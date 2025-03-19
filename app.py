01b14d6089d55cfa999fb0cee55e8d78775dae17
Hassan bin Aman <140601000+player-ZED@users.noreply.github.com>
2025-03-19 23:46:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def scrape_product():
                     elif "pdp-callout-outlet-nopromo" in callout_ids:
                         # discounted product
                         # Adjust prices for outlet items
-                        sku_price = current_price +2
+                        sku_price = current_price + 2
                         selling_price = sku_price + 1.199
 
                 result = {
